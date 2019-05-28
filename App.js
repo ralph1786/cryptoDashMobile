5cf39f0af5783ae12f9674a1ef4f86c5b8bdad03
ralph1786 <ralph1786@gmail.com>
2019-05-28 12:56:37 -0400

    added redux to screens

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
+import React from "react";
 import { Navigation } from "react-native-navigation";
 import DashboardScreen from "./src/screens/Dashboard";
 import HomeScreen from "./src/screens/Home";
 import WelcomeScreen from "./src/screens/Welcome";
+import storeConfig from "./src/store/storeConfig";
+import { Provider } from "react-redux";
+
+const store = storeConfig();
 
 //Screen Registration
-Navigation.registerComponent("cryptoDash-home", () => HomeScreen);
-Navigation.registerComponent("cryptoDash-dashboard", () => DashboardScreen);
+Navigation.registerComponent(
+  "cryptoDash-home",
+  () => props => (
+    <Provider store={store}>
+      <HomeScreen {...props} />
+    </Provider>
+  ),
+  () => HomeScreen
+);
+Navigation.registerComponent(
+  "cryptoDash-dashboard",
+  () => props => (
+    <Provider store={store}>
+      <DashboardScreen {...props} />
+    </Provider>
+  ),
+  () => DashboardScreen
+);
 Navigation.registerComponent("cryptoDash-welcome", () => WelcomeScreen);
 
 //Start screen for app
