874d7e21c7f343acebb57efa88f48d97d7e22da6
ralph1786 <ralph1786@gmail.com>
2019-05-30 11:58:40 -0400

    added animation, styling, redirect, redux

--- src/components/CryptoCard.js
+++ src/components/CryptoCard.js
@@ -1,20 +1,82 @@
-import React from "react";
-import { Card, CardItem, Icon, Text, Right } from "native-base";
-import { Image } from "react-native";
+import React, { Component } from "react";
+import { Image, View, StyleSheet, TouchableOpacity, Text } from "react-native";
 import currencyImage from "./currencyImage";
+import { selectedCurrency } from "../store/actions";
+import { connect } from "react-redux";
+import FadeInAnimation from "./FadeInAnimation";
+import { Navigation } from "react-native-navigation";
 
-const CryptoCard = props => {
-  return (
-    <Card>
-      <CardItem>
-        <Image source={currencyImage(props.currency.symbol)} />
-        <Text>{props.currency}</Text>
-        <Right>
-          <Icon name="arrow-forward" />
-        </Right>
-      </CardItem>
-    </Card>
-  );
+class CryptoCard extends Component {
+  onPressHandler = () => {
+    this.props.selectedCurrency(this.props.currency);
+    Navigation.push(this.props.componentId, {
+      component: {
+        name: "cryptoDash-dashboard",
+        options: {
+          topBar: {
+            title: {
+              text: "Dashboard",
+              color: "blue"
+            }
+          }
+        }
+      }
+    });
+  };
+
+  render() {
+    return (
+      <TouchableOpacity onPress={() => this.onPressHandler()}>
+        <FadeInAnimation>
+          <View style={styles.container}>
+            <Image
+              style={styles.imageStyling}
+              source={{ uri: currencyImage(this.props.currency.currency) }}
+            />
+            <Text style={styles.textStyling}>
+              {this.props.currency.currency}
+            </Text>
+            <Text style={styles.priceStyling}>
+              $ {this.props.currency.price.slice(0, 7)}
+            </Text>
+          </View>
+        </FadeInAnimation>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    padding: 10,
+    margin: 5,
+    backgroundColor: "#99c8f7",
+    flexDirection: "row",
+    alignItems: "center",
+    height: 90
+  },
+  imageStyling: {
+    width: 70,
+    height: 60,
+    marginLeft: 20
+  },
+  textStyling: {
+    marginLeft: 30
+  },
+  priceStyling: {
+    marginLeft: 40,
+    color: "red"
+  }
+});
+
+const mapDispatchToProps = dispatch => {
+  return {
+    selectedCurrency: currency => dispatch(selectedCurrency(currency))
+  };
 };
 
-export default CryptoCard;
+export default connect(
+  null,
+  mapDispatchToProps
+)(CryptoCard);
