4e248c5ac40c837ae49a13bfb268678e1f49513e
ralph1786 <ralph1786@gmail.com>
2019-05-30 12:01:27 -0400

    changed to stack layout

--- src/navigation/navigationTabs.js
+++ src/navigation/navigationTabs.js
@@ -2,18 +2,58 @@ import { Navigation } from "react-native-navigation";
 import homeIcon from "../assets/icons/homeicon.png";
 import dashboardIcon from "../assets/icons/dashboard.png";
 
+// const navigationTabs = () => {
+//   Navigation.setRoot({
+//     root: {
+//       bottomTabs: {
+//         id: "BottomTabsId",
+//         children: [
+//           {
+//             component: {
+//               name: "cryptoDash-home",
+//               options: {
+//                 bottomTab: {
+//                   fontSize: 12,
+//                   text: "Home",
+//                   icon: homeIcon
+//                 }
+//               }
+//             }
+//           },
+//           {
+//             component: {
+//               name: "cryptoDash-dashboard",
+//               options: {
+//                 bottomTab: {
+//                   text: "Dashboard",
+//                   fontSize: 12,
+//                   icon: dashboardIcon
+//                 }
+//               }
+//             }
+//           }
+//         ]
+//       }
+//     }
+//   });
+// };
+
 const navigationTabs = () => {
   Navigation.setRoot({
     root: {
       bottomTabs: {
-        id: "BottomTabsId",
         children: [
           {
-            component: {
-              name: "cryptoDash-home",
+            stack: {
+              children: [
+                {
+                  component: {
+                    name: "cryptoDash-home"
+                  }
+                }
+              ],
               options: {
                 bottomTab: {
-                  fontSize: 12,
                   text: "Home",
                   icon: homeIcon
                 }
@@ -23,10 +63,10 @@ const navigationTabs = () => {
           {
             component: {
               name: "cryptoDash-dashboard",
+
               options: {
                 bottomTab: {
                   text: "Dashboard",
-                  fontSize: 12,
                   icon: dashboardIcon
                 }
               }
