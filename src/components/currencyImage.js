5d92db67d7dee8bc322a4b93ab1bd202e8f69e26
ralph1786 <ralph1786@gmail.com>
2019-07-05 12:39:45 -0400

    added more images

--- src/components/currencyImage.js
+++ src/components/currencyImage.js
@@ -7,11 +7,19 @@ const currencyImage = symbol => {
     case "XRP":
       return "https://image.spreadshirtmedia.com/image-server/v1/mp/designs/1016387854,width=178,height=178,version=1523654712/ripple-xrp-new-logo-crypto-currency-bitcoin-hodl.png";
     case "BCH":
-      return "https://www.bitcoin.com/bitcoin-cash-brand/media-kit/green/3-bitcoin-cash-logo-ot-medium.png";
+      return "https://s.yimg.com/ny/api/res/1.2/G9jAmuZRb34GvCCi4Oisgw--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/http://media.zenfs.com/en-US/homerun/coin_rivet_596/62ba09746ed3275948a453948f052c71";
     case "LTC":
       return "https://static1.squarespace.com/static/5a78883ba803bb706feafd92/t/5a82200008522962b660c976/1518474968211/?format=1500w";
     case "EOS":
       return "https://seeklogo.com/images/E/eos-logo-ECF31E0936-seeklogo.com.png";
+    case "BNB":
+      return "https://cdn-images-1.medium.com/max/1600/1*HS1k7-dZFTv7I-Gau7Hlvw.png";
+    case "USDT":
+      return "https://image.flaticon.com/icons/png/512/825/825508.png";
+    case "BSV":
+      return "https://pbs.twimg.com/profile_images/1051964386699563011/wUjCaoil_400x400.jpg";
+    case "ADA":
+      return "https://qolczpnfu7-flywheel.netdna-ssl.com/wp-content/uploads/2017/12/cardano_cryptocurrency_800x480.jpg";
     default:
       break;
   }
